46607d9406cc5ac18e6d5b9183bd8ef30bed2b93
andhavarapuvamsikrishna <andhavarapuvamsikrishna@gmail.com>
2020-07-24 15:08:32 +0530

    last stage of application

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,5 @@
 import React,{ useState,useEffect} from "react"
-import {NativeSelect,FormControl } from '@material-ui/core'
+//import {NativeSelect,FormControl } from '@material-ui/core'
 import styles from "./CountryPicker.module.css"
 import { fetchCountries } from "../../api"
 const CountryPicker=({ handleCountryChange }) => {
