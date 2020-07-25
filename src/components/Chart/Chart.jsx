1752cb1dc16b54284ff7276ad96258b35f28d6f5
andhavarapuvamsikrishna <andhavarapuvamsikrishna@gmail.com>
2020-07-26 00:10:02 +0530

    font colors and sizes of graphs have been changes

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -23,7 +23,7 @@ const Chart=({data,country}) => {
             datasets:[{
                   data:dailyData.map((data) => data.confirmed),
                   label:'Infected',
-                  borderColor: "#3333ff",
+                  borderColor: "#1a66ff",
                   fill:true, 
             },{
                 data:dailyData.map((data) => data.deaths),
@@ -34,7 +34,35 @@ const Chart=({data,country}) => {
             },],
         }}
         options={{
-            maintainAspectRatio:false
+            maintainAspectRatio:false,
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {   // To format the scale Lebel
+                        display: true,
+                        //labelString: 'X axe name',
+                        fontColor:'white',
+                        fontSize:10
+                    },
+                    ticks: {
+                       fontColor: "#d1c5c5", // To format the ticks, coming on the axis/lables which we are passing.
+                       fontSize: 14
+                      }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {   // To format the scale Lebel
+                        display: true,
+                        labelString: '<-----Number of people---->',
+                        fontColor:'#d1c5c5',
+                        fontSize:10
+                    },
+                    ticks: {
+                       fontColor: "#d1c5c5", // To format the ticks, coming on the axis/lables which we are passing.
+                       fontSize: 14
+                      }
+                }]
+            }
         }}
         
         />):null
@@ -47,14 +75,43 @@ const Chart=({data,country}) => {
                     labels:['infected','recovered','Deaths'],
                     datasets:[{
                         label:'people',
-                        backgroundColor:["rgba(0, 0,255, 0.5)","rgba(0, 255,0, 0.5)","rgba(255, 0,0, 0.5)"],
+                        backgroundColor:["steelblue","rgba(0, 255,0, 0.5)","red"],
                         data:[data.confirmed.value,data.recovered.value,data.deaths.value]
                     }]
                 }}
                 options={{
                     legend:{display:false},
                     title:{display:true,text:`current state in ${country}`},
-                    
+                    maintainAspectRatio:false,
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {   // To format the scale Lebel
+                                display: true,
+                                //labelString: 'X axe name',
+                                fontColor:'white',
+                                fontSize:10
+                            },
+                            ticks: {
+                               fontColor: "#d1c5c5", // To format the ticks, coming on the axis/lables which we are passing.
+                               fontSize: 13
+                              }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {   // To format the scale Lebel
+                                display: true,
+                                labelString: '<-----Number of people---->',
+                                fontColor:'#d1c5c5',
+                                fontSize:10
+                            },
+                            ticks: {
+                               fontColor: "#d1c5c5", // To format the ticks, coming on the axis/lables which we are passing.
+                               fontSize: 14
+                              }
+                        }]
+                    }
+
                 }}
             />
         ): null
